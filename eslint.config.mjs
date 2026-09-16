b3885334e65143de8914911a660c238fca69c9e6
Hariom Kashyap <omkashyap6870@gmail.com>
2026-09-16 20:25:36 +0530

    fix: fix eslint react version error & add new script to pre-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,13 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    settings: {
+      react: {
+        version: "19"
+      }
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -17,6 +24,7 @@ const eslintConfig = defineConfig([
     ".claude/**",
     ".devin/**",
     ".cursor/**",
+    "src/prisma/contract.d.ts"
   ]),
 ]);
 
